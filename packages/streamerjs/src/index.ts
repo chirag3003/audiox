9cf98df62d1fc00ee9a7deb52f499bdbfc784e41
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-01 04:14:37 +0530

    setup webhooks

--- packages/streamerjs/src/index.ts
+++ packages/streamerjs/src/index.ts
@@ -1,1 +1,10 @@
 console.log("Starting");
+import WebSocket, { WebSocketServer } from "ws";
+
+const ws = new WebSocketServer({
+    port: 8080,
+});
+
+ws.on("connection", e => {
+    e.send("hello");
+});
